fe1f36facb20221f7d1d237234696149f87ba954
EsraaMokhtar <89299244+EsraaMokhtar@users.noreply.github.com>
2023-05-03 12:32:48 +0300

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,36 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const runtimeCaching = require("next-pwa/cache");
+// const withPWA = require('next-pwa');
+// const nextConfig = withPWA({
+//   reactStrictMode: true,
+//   pwa:{
+//     dest: 'public',
+//     register:true,
+//     skipWaiting:true,
+//     disable:process.env.NODE_ENV === 'development',
+//     runtimeCaching,
+//     buildExcludes: [/middleware-manifest.json$/],
+//   }
+// });
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+// Use `withPWA` and pass general Next.js config
+module.exports = withPWA({    
+  reactStrictMode: true,
+  webpack5: true,
+  webpack: (config) => {
+      config.resolve.fallback = { fs: false };
+      return config;
+  },
+  images: {
+      domains: ["pbs.twimg.com", "img.icons8.com", "gateway.moralisipfs.com", "ipfs.moralis.io", "lh3.googleusercontent.com", "www.artnews.com"]
+  },
+  output: "standalone"
+});
